27ed50970e45c7ea07b0ad9d8f5b4191b06822e0
Benjamin <bkearney@intelcomexpress.com>
2021-01-18 14:20:41 -0500

    update package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="package_test",
-    version="0.0.2",
+    version="0.0.3",
     author="bkearney",
     author_email="bkearney@intelcomexpress.com",
     description="A small example package",

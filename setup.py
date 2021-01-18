0caa18e18df50af1e2e7d96bcf710adc9c2fc317
Benjamin <bkearney@intelcomexpress.com>
2021-01-18 13:02:53 -0500

    add test project

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="package_test",
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'python_hello@git+https://github.com/ceddlyburge/python_hello#egg=python_hello',
+        'boto3==1.14.53',
+    ]
+)
